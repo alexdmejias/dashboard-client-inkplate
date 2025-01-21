8e94b4ba24fd07807b60410d09f9dafd9bdab3c7
Alex Mejias <alexdmejias@gmail.com>
2025-01-20 21:28:04 -0500

    works after rewrite

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,79 @@
-#ifndef ARDUINO_INKPLATE10
-#error "Wrong board selection for this example, please select Inkplate 10 in the boards menu."
+// Next 3 lines are a precaution, you can ignore those, and the example would also work without them
+#if !defined(ARDUINO_INKPLATE10) && !defined(ARDUINO_INKPLATE10V2)
+#error "Wrong board selection for this example, please select e-radionica Inkplate10 or Soldered Inkplate10 in the boards menu."
 #endif
 
-#define ESP_DRD_USE_SPIFFS true
-
-#include "Inkplate.h" //Include Inkplate library to the sketch
-// #include "SdFat.h"    //Include library for SD card
-#define FS_NO_GLOBALS
-#include <FS.h>
-#include <SPIFFS.h>
-#include "driver/rtc_io.h"
+#include "Inkplate.h"
+#include "SdFat.h"
 #include "Arduino.h"
-
-#include "globals.h"
-// #include "draw.h"
-// #include "network.h"
-
-#include <Update.h>
-#include <WebServer.h>
-#include <DNSServer.h>
-#include <WiFiManager.h>
-// #include <ESP_DoubleResetDetector.h>
 #include <ArduinoJson.h>
-#include <HTTPClient.h>
+#include "HTTPClient.h" //Include library for HTTPClient
+#include "WiFi.h"       //Include library for WiFi
 
 Inkplate display(INKPLATE_3BIT);
 SdFile file;
-// Draw d;
-WiFiManager wm;
 
-// Number of seconds after reset during which a
-// subseqent reset will be considered a double reset.
-#define DRD_TIMEOUT 10
-
-// RTC Memory Address for the DoubleResetDetector to use
-#define DRD_ADDRESS 0
-
-// DoubleResetDetector *drd;
-
-char image_server[34] = "";
-
-const long interval = 5000;
-
-// flag for saving data
-bool shouldSaveConfig = false;
-
-void saveConfigCallback()
+struct Config
 {
-  Serial.println("Should save config");
-  shouldSaveConfig = true;
+  char hostname[64];
+  char password[64];
+  char ssid[64];
+};
+
+const char *filename = "/config.txt"; // <- SD library uses 8.3 filenames
+Config config;                        // <- global configuration object
+
+void readConfig(const char *filename, Config &config);
+void drawCentreString(Inkplate &d, String buf);
+
+void setup()
+{
+  display.begin();        // Init Inkplate library (you should call this function ONLY ONCE)
+  display.clearDisplay(); // Clear frame buffer of display
+  display.display();      // Put clear image on display
+  display.setTextSize(5);
+
+  Serial.begin(230400); // Init serial communication
+  Serial.println(":::::::::::::::::::: Inkplate SD card example");
+  readConfig(filename, config);
+
+  Serial.println(":::::::::::::::::::: Connecting to WiFi...");
+
+  // Connect to the WiFi network.
+  WiFi.mode(WIFI_MODE_STA);
+  WiFi.begin(config.ssid, config.password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(":::::::::::::::::::: WiFi OK! Downloading...");
+
+  Serial.println(":::::::::::::::::::: config data:");
+  Serial.println("hostname");
+  Serial.println(config.hostname);
+  Serial.println("ssid");
+  Serial.println(config.ssid);
+  Serial.println("password");
+  Serial.println(config.password);
+  Serial.println(":::::::::::::::::::: will display image promptly");
+
+  if (!display.drawPngFromWeb(config.hostname, 0, 0, 0, true))
+  {
+    Serial.println(":::::::::::::::::::: Image open error");
+    display.display();
+  }
+  Serial.println(":::::::::::::::::::: Image displayed");
+  drawCentreString(display, String(config.hostname));
+  display.display();
+
+  display.sdCardSleep(); // Put sd card in sleep mode
 }
 
-void saveConfigFile()
+void loop()
 {
-  Serial.println("saving config...");
-
-  DynamicJsonDocument jsonBuffer(1024);
-
-  jsonBuffer["image_server"] = image_server;
-
-  fs::File configFile = SPIFFS.open("/config.json", "w");
-  if (!configFile)
-  {
-    Serial.println("failed to open config file for writing");
-  }
-
-  serializeJson(jsonBuffer, Serial);
-  serializeJson(jsonBuffer, configFile);
-
-  configFile.close();
+  // Nothing...
 }
 
 void drawCentreString(Inkplate &d, String buf)
@@ -80,236 +85,54 @@ void drawCentreString(Inkplate &d, String buf)
   d.getTextBounds(buf, 600, 790, &x1, &y1, &w, &h); // calc width of new string
   d.setCursor(600 - w / 2, 790);
   d.println(buf);
-  d.display();
 }
 
-// Loads custom parameters from /config.json on SPIFFS
-bool loadConfigFile()
+void readConfig(const char *filename, Config &config)
 {
-  Serial.println("mounting FS...");
-
-  if (SPIFFS.begin())
+  // Init SD card. Display if SD card is init propery or not.
+  if (display.sdCardInit())
   {
-    Serial.println("mounted file system");
-    if (SPIFFS.exists("/config.json"))
+    Serial.println(":::::::::::::::::::: SD Card ok! Reading data...");
+    // Allocate a temporary JsonDocument
+    JsonDocument doc;
+
+    // Try to load text with max lenght of 200 chars.
+    if (!file.open("/config.txt", O_RDONLY))
+    { // If it fails to open, send error message to display, otherwise read the file.
+      Serial.println(":::::::::::::::::::: File open error");
+      display.display();
+      display.sdCardSleep();
+    }
+    else
     {
-      // file exists, reading and loading
-      Serial.println("reading config file");
-      fs::File configFile = SPIFFS.open("/config.json", "r");
-      if (configFile)
+      char text[201];            // Array where data from SD card is stored (max 200 chars here)
+      int len = file.fileSize(); // Read how big is file that we are opening
+      if (len > 200)
+        len = 200;          // If it's more than 200 bytes (200 chars), limit to max 200 bytes
+      file.read(text, len); // Read data from file and save it in text array
+      text[len] = 0;        // Put null terminating char at the and of data
+
+      DeserializationError error = deserializeJson(doc, text);
+      if (error)
       {
-        Serial.println("opened config file");
-        size_t size = configFile.size();
-        // Allocate a buffer to store contents of the file.
-        std::unique_ptr<char[]> buf(new char[size]);
-
-        configFile.readBytes(buf.get(), size);
-
-        DynamicJsonDocument json(1024);
-        auto deserializeError = deserializeJson(json, buf.get());
-        serializeJson(json, Serial);
-        if (!deserializeError)
-        {
-          Serial.println("\nparsed json");
-          strcpy(image_server, json["image_server"]);
-
-          return true;
-        }
-        else
-        {
-          Serial.println("failed to load json config");
-        }
-        configFile.close();
+        Serial.println(F("Failed to read file, using default configuration"));
       }
+
+      Serial.println(":::::::::::::::::::: did not fail to read config file, copying values to config object");
+
+      // Copy values from the JsonDocument to the Config
+      // config.port = doc["port"] | 1234;
+
+      strlcpy(config.hostname, doc["hostname"] | "example.com", sizeof(config.hostname));
+      strlcpy(config.ssid, doc["ssid"] | "example.com", sizeof(config.hostname));
+      strlcpy(config.password, doc["password"] | "example.com", sizeof(config.hostname));
     }
   }
   else
-  {
-    Serial.println("failed to mount FS");
+  { // If card init was not successful, display error on screen, put sd card in sleep mode, and stop the program (using infinite loop)
+    Serial.println(":::::::::::::::::::: SD Card error!");
+    display.sdCardSleep();
+    while (true)
+      ;
   }
-
-  return false;
-}
-
-// This gets called when the config mode is launced, might
-// be useful to update a display with this info.
-void configModeCallback(WiFiManager *myWiFiManager)
-{
-  Serial.println("Entered Conf Mode");
-
-  Serial.print("Config SSID: ");
-  Serial.println(myWiFiManager->getConfigPortalSSID());
-
-  Serial.print("Config IP Address: ");
-  Serial.println(WiFi.softAPIP());
-}
-
-void setup()
-{
-  SPIFFS.begin(true);
-  Serial.begin(115200);
-  display.begin();
-
-  display.clearDisplay();
-  Serial.setDebugOutput(true);
-
-  bool forceConfig = false;
-
-  // bool spiffsSetup = loadConfigFile();
-  DynamicJsonDocument json(1024);
-
-  if (SPIFFS.begin())
-  {
-    Serial.println("mounted file system");
-    if (SPIFFS.exists("/config.json"))
-    {
-      // file exists, reading and loading
-      Serial.println("reading config file");
-      fs::File configFile = SPIFFS.open("/config.json", "r");
-      if (configFile)
-      {
-        Serial.println("opened config file");
-        size_t size = configFile.size();
-        // Allocate a buffer to store contents of the file.
-        std::unique_ptr<char[]> buf(new char[size]);
-
-        configFile.readBytes(buf.get(), size);
-
-        auto deserializeError = deserializeJson(json, buf.get());
-        serializeJson(json, Serial);
-        if (!deserializeError)
-        {
-          Serial.println("\nparsed json");
-          Serial.println(json["image_server"].as<const char *>());
-          strcpy(image_server, json["image_server"]);
-
-          // return true;
-        }
-        else
-        {
-          Serial.println("failed to load json config");
-        }
-        // configFile.close();
-      }
-    }
-  }
-  else
-  {
-    Serial.println("failed to mount FS");
-  }
-
-  WiFiManagerParameter custom_image_server("server", "image server", image_server, 34);
-
-  // if (!spiffsSetup)
-  // {
-  //   Serial.println(F("Forcing config mode as there is no saved config"));
-  //   forceConfig = true;
-  // }
-
-  if (display.readTouchpad(PAD1))
-  {
-    Serial.println(F("Forcing config mode because PAD1 is being pressed"));
-    forceConfig = true;
-  }
-
-  // WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
-
-  // wm.setSaveConfigCallback(saveConfigCallback);
-  // set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
-  // wm.setAPCallback(configModeCallback);
-
-  wm.addParameter(&custom_image_server);
-
-  if (forceConfig)
-  {
-    if (!wm.startConfigPortal("::inkplate-forced"))
-    {
-      Serial.println("forcing manual config");
-      drawCentreString(display, String("forcing manual config"));
-      delay(3000);
-      // reset and try again, or maybe put it to deep sleep
-      ESP.restart();
-      delay(5000);
-    }
-  }
-  else
-  {
-    if (!wm.autoConnect("::inkplate"))
-    {
-      Serial.println("failed to connect and hit timeout");
-      drawCentreString(display, String("failed to connect and hit timeout"));
-      delay(3000);
-      // if we still have not connected restart and try all over again
-      ESP.restart();
-      delay(5000);
-    }
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  // strcpy(image_server, custom_image_server);
-
-  Serial.println("connected...yeey :)");
-  // Serial.println(">>>>>" + json["custom_image_server"] + "<<<<<");
-  // Serial.println(">>>>>" + String(image_server) + "<<<<<");
-  strcpy(image_server, custom_image_server.getValue());
-  Serial.println(">>>>>" + String(image_server) + "<<<<<");
-
-  if (shouldSaveConfig)
-  {
-    Serial.println("saving config");
-    json["image_server"] = image_server;
-
-    fs::File configFile = SPIFFS.open("/config.json", "w");
-    if (!configFile)
-    {
-      Serial.println("failed to open config file for writing");
-    }
-
-    serializeJson(json, Serial);
-    serializeJson(json, configFile);
-    configFile.close();
-    // end save
-  }
-
-  // save the custom parameters to FS
-  if (shouldSaveConfig)
-  {
-    saveConfigFile();
-  }
-
-  // d.update(display, String(image_server));
-
-  display.drawImage(image_server, 0, 0, 0, 1);
-
-  drawCentreString(display, String(image_server));
-  // display.drawBitmapFromWeb()
-  display.display();
-
-  // DO NOT DELETE
-  esp_sleep_enable_timer_wakeup(globals::TIME_TO_SLEEP * globals::uS_TO_S_FACTOR); // Activate wake-up timer
-  esp_deep_sleep_start();                                                          // Put ESP32 into deep sleep. Program stops here.
-
-  Serial.println("End setup");
-}
-
-// unsigned long previousMillis = 0;
-
-void loop()
-{
-
-  // unsigned long currentMillis = millis();
-
-  // Serial.println("in loop");
-
-  // if (currentMillis - previousMillis >= interval)
-  // {
-  //   // save the last time you blinked the LED
-  //   previousMillis = currentMillis;
-
-  //   d.update(display, custom_image_server.getValue());
-  // }
 }
