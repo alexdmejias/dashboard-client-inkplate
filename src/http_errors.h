cb86b3c5c6feea9f3b94cde4a42a2eca4ff328e0
Alex Mejias <alexdmejias@gmail.com>
2026-01-06 19:54:06 -0500

    fixing build issue

--- src/http_errors.h
+++ src/http_errors.h
@@ -2,20 +2,7 @@
 
 #include "Arduino.h"
 #include "Inkplate.h"
-
-// ESP32 HTTPClient error codes
-// Source: https://github.com/espressif/arduino-esp32/blob/master/libraries/HTTPClient/src/HTTPClient.h
-const int HTTPC_ERROR_CONNECTION_REFUSED = -1;
-const int HTTPC_ERROR_SEND_HEADER_FAILED = -2;
-const int HTTPC_ERROR_SEND_PAYLOAD_FAILED = -3;
-const int HTTPC_ERROR_NOT_CONNECTED = -4;
-const int HTTPC_ERROR_CONNECTION_LOST = -5;
-const int HTTPC_ERROR_NO_STREAM = -6;
-const int HTTPC_ERROR_NO_HTTP_SERVER = -7;
-const int HTTPC_ERROR_TOO_LESS_RAM = -8;
-const int HTTPC_ERROR_ENCODING = -9;
-const int HTTPC_ERROR_STREAM_WRITE = -10;
-const int HTTPC_ERROR_READ_TIMEOUT = -11;
+#include "HTTPClient.h"
 
 /**
  * Handle HTTP errors and display appropriate user-friendly messages
